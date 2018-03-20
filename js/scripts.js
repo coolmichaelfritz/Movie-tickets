6e7789527f0e8c92cc7321f6f19567aafdb6dca5
Janice Nam and Michael Fritz <janice@>
2018-03-19 20:21:01 -0700

    make movie ticket selector using Constructor

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+//Business logic:
+
+function Ticket(movieName, time, age) {
+  this.movieName = movieName;
+  this.time = time;
+  this.age = age;
+}
+
+
+//User interface logic:
+
+$(document).ready(function() {
+
+  $("form#movies").submit(function(event) {
+    event.preventDefault();
+debugger;
+    var question1 = $("select#movieSelection").val();
+    var question2 = $("select#movieTime").val();
+    var question3 = $("select#movieAge").val();
+
+    $("#showtime").text(question2);
+
+    $("#results").show();
+
+    var newTicket = new Ticket(question1, question2, question3);
+
+    var moviePrice = 0;
+
+    if (newTicket.time === "12:00PM") {
+      moviePrice += 10
+    } else if (newTicket.time === "7:00PM") {
+      moviePrice += 15
+    }
+
+    if (newTicket.age === "yes") {
+      moviePrice -=5
+    }
+
+    $("#showtimePrice").text(moviePrice);
+
+
+
+    if (newTicket.movieName === "blackPanther") {
+      $("#movieImage").append("<img src='img/blackpanther.jpg'class='img-responsive'>");
+    } else if (newTicket.movieName === "thor") {
+      $("#movieImage").append("<img src='img/thor.jpg' class='img-responsive'>");
+    } else if (newTicket.movieName === "inception") {
+      $("#movieImage").append("<img src='img/inception.jpg' class='img-responsive'>");
+    }
+  });
+});
